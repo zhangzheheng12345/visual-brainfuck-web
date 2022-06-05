b3790970ae145506f3f69d9507665da87a91c513
Zheheng Zhang <76390451+zhangzheheng12345@users.noreply.github.com>
2022-06-05 08:52:23 +0800

    Fix that +-,.><[] in comment will not be minified

--- run.js
+++ run.js
@@ -1,5 +1,13 @@
 function minify(origin){
-    return origin.match(/[\+\-><\[\],\.]+/g).join("")
+    let commentRemoved = ""
+    // Avoid +-,.><[] in comment not being minified
+    for(let i = 0; i < origin.length; i++) {
+        if(origin[i] == "#")
+            while(origin[i] != "\n") i++;
+        else
+            commentRemoved += origin[i];
+    }
+    return commentRemoved.match(/[\+\-><\[\],\.]+/g).join("")
 }
 
 // Run program function
