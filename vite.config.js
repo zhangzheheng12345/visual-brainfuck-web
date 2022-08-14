59b4394662f2be40155045dd97d156ebbaf36dce
zhangzheheng12345@163.com <zhangzheheng12345@163.com>
2022-08-14 22:15:10 +0800

    Change base for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue"
 import "path"
 
 export default defineConfig({
-  base: "/visual-brainfuck-web/",
+  base: "",
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
