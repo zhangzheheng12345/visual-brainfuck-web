b708a83557df23685d7c0b0ed67b67dce4f60e89
zhangzheheng12345@163.com <zhangzheheng12345@163.com>
2023-01-04 15:54:12 +0800

    Use Google fonts

--- src/components/mainBox/dataArea.vue
+++ src/components/mainBox/dataArea.vue
@@ -1,83 +1,90 @@
 <template>
-    <div id="wrapData">
-        <button id="showData" :class="{ toRotate: !dataHidden }" @click="$emit('toggleData')" :title="dataHidden?'show the data area' : 'hide the data area'">
-            <img src="./svgs/triangle.svg" />
-        </button>
-        <div id="data" v-show="!dataHidden">
-            <div v-for="(item, index) in data" :class="{ selected: (ptr == index) }">{{ item }}</div>
-        </div>
-        <div id="dataHiddenReminder" v-show="dataHidden">DATA AREA HIDDEN</div>
+  <div id="wrapData">
+    <button
+      id="showData"
+      :class="{ toRotate: !dataHidden }"
+      @click="$emit('toggleData')"
+      :title="dataHidden ? 'show the data area' : 'hide the data area'"
+    >
+      <img src="./svgs/triangle.svg" />
+    </button>
+    <div id="data" v-show="!dataHidden">
+      <div v-for="(item, index) in data" :class="{ selected: ptr == index }">
+        {{ item }}
+      </div>
     </div>
+    <div id="dataHiddenReminder" v-show="dataHidden">DATA AREA HIDDEN</div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ["data", "ptr", "dataHidden"]
-}
+  props: ["data", "ptr", "dataHidden"],
+};
 </script>
 
 <style scoped>
 #showData {
-    height: 25px;
-    width: 25px;
-    margin-right: 10px;
-    margin-top: 10px;
-    padding: 2px;
-    background-color: var(--very-dark-grey);
-    border-width: 0px;
-    border-radius: 0px;
+  height: 25px;
+  width: 25px;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 2px;
+  background-color: var(--very-dark-grey);
+  border-width: 0px;
+  border-radius: 0px;
 }
 
 .toRotate {
-    transform: rotate(90deg);
+  transform: rotate(90deg);
 }
 
 #wrapData {
-    display: flex;
-    margin: 10px;
-    margin-left: 1px;
-    min-height: 42px;
+  display: flex;
+  margin: 10px;
+  margin-left: 1px;
+  min-height: 42px;
 }
 
 #data {
-    min-height: 38px;
-    background-color: var(--grey);
-    padding: 5px;
-    margin-left: 5px;
-    border-radius: 7px;
-    border-width: 1px;
-    border-color: #fff;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  min-height: 38px;
+  background-color: var(--grey);
+  padding: 5px;
+  margin-left: 5px;
+  border-radius: 7px;
+  border-width: 1px;
+  border-color: #fff;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 
-#data>div {
-    background-color: #fff;
-    margin: 4px;
-    padding: 3px;
-    width: 40px;
-    border-radius: 5px;
-    text-align: center;
-    color: #000;
-    font-size: 17px;
-    font-family: sans-serif;
-    font-weight: bold;
-    cursor: default;
+#data > div {
+  background-color: #fff;
+  margin: 4px;
+  padding: 3px;
+  width: 40px;
+  border-radius: 5px;
+  text-align: center;
+  color: #000;
+  font-size: 17px;
+  font-family: sans-serif;
+  font-weight: bold;
+  cursor: default;
 }
 
-#data>div.selected {
-    transform: scale(1.1, 1.2);
+#data > div.selected {
+  transform: scale(1.1, 1.2);
 }
 
 #dataHiddenReminder {
-    padding: 10px;
-    margin: 3px;
-    font-size: 20px;
-    color: #fff;
-    font-family: "Ubuntu Mono", Courier, Menlo, Consolas, monospace;
-    background-color: var(--dark-grey);
-    border-radius: 5px;
-    cursor: default;
+  padding: 10px;
+  margin: 3px;
+  font-size: 20px;
+  color: #fff;
+  font-family: "Ubuntu", Courier, Menlo, Consolas, monospace;
+  background-color: var(--dark-grey);
+  border-radius: 5px;
+  cursor: default;
 }
 </style>
--- src/components/topBar.vue
+++ src/components/topBar.vue
@@ -1,100 +1,106 @@
 <template>
-    <div id="topBarReplacer"></div>
-    <div id="topBar">
-        <img id="bfLogo" src="./svgs/v-bf-web-logo.svg" type="image/svg+xml" />
-        <button id="menuButton" @click="toggleMenu()">
-            <img src="./svgs/menu.svg" type="image/svg+xml" />
-        </button>
-        <div id="menu" v-show="!menuHidden">
-            <div><a href="https://github.com/zhangzheheng12345/visual-brainfuck-web">GitHub Repo</a></div>
-            <div><a href="https://github.com/zhangzheheng12345">My Profile</a></div>
-            <div><a href="https://esolangs.org/wiki/brainfuck">Brainfuck Wiki</a></div>
-        </div>
+  <div id="topBarReplacer"></div>
+  <div id="topBar">
+    <img id="bfLogo" src="./svgs/v-bf-web-logo.svg" type="image/svg+xml" />
+    <button id="menuButton" @click="toggleMenu()">
+      <img src="./svgs/menu.svg" type="image/svg+xml" />
+    </button>
+    <div id="menu" v-show="!menuHidden">
+      <div>
+        <a href="https://github.com/zhangzheheng12345/visual-brainfuck-web"
+          >GitHub Repo</a
+        >
+      </div>
+      <div><a href="https://github.com/zhangzheheng12345">My Profile</a></div>
+      <div>
+        <a href="https://esolangs.org/wiki/brainfuck">Brainfuck Wiki</a>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            menuHidden: true
-        }
+  data() {
+    return {
+      menuHidden: true,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuHidden = !this.menuHidden;
     },
-    methods: {
-        toggleMenu() {
-            this.menuHidden = !this.menuHidden
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 #topBarReplacer {
-    width: 100%;
-    height: 40px;
-    background-color: var(--extremely-dark-grey);
+  width: 100%;
+  height: 40px;
+  background-color: var(--extremely-dark-grey);
 }
 
 #topBar {
-    width: 100%;
-    height: 40px;
-    background-color: var(--very-dark-grey);
-    padding: 3px;
-    position: fixed;
-    right: 0px;
-    top: 0px;
-    z-index:99;
+  width: 100%;
+  height: 40px;
+  background-color: var(--very-dark-grey);
+  padding: 3px;
+  position: fixed;
+  right: 0px;
+  top: 0px;
+  z-index: 99;
 }
 
 #bfLogo {
-    transform-origin: 20px -3px;
-    transform: scale(0.05, 0.043);
+  transform-origin: 20px -3px;
+  transform: scale(0.05, 0.043);
 }
 
 #menuButton {
-    border-width: 0px;
-    background-color: var(--very-dark-grey);
-    padding: 6px;
-    position: absolute;
-    top: 2px;
-    right: 5px;
-    z-index: 99;
+  border-width: 0px;
+  background-color: var(--very-dark-grey);
+  padding: 6px;
+  position: absolute;
+  top: 2px;
+  right: 5px;
+  z-index: 99;
 }
 
 #menuButton:hover {
-    transform: scale(1.1, 1.1);
+  transform: scale(1.1, 1.1);
 }
 
-#menuButton>* {
-    transform: scale(1.5, 1.6);
+#menuButton > * {
+  transform: scale(1.5, 1.6);
 }
 
 #menu {
-    position: absolute;
-    right: 5px;
-    top: 55px;
-    border-radius: 8px;
-    padding: 5px;
-    background-color: var(--grey);
-    z-index: 99;
+  position: absolute;
+  right: 5px;
+  top: 55px;
+  border-radius: 8px;
+  padding: 5px;
+  background-color: var(--grey);
+  z-index: 99;
 }
 
-#menu>div {
-    background-color: var(--very-dark-grey);
-    border-radius: 5px;
-    margin: 8px;
-    padding: 5px;
+#menu > div {
+  background-color: var(--very-dark-grey);
+  border-radius: 5px;
+  margin: 8px;
+  padding: 5px;
 }
 
-#menu>div>a {
-    color: #fff;
-    font-size: 17px;
-    font-family: "League Mono", Courier, Menlo, Consolas, monospace;
-    text-decoration: none;
-    padding: 5px;
+#menu > div > a {
+  color: #fff;
+  font-size: 17px;
+  font-family: "IBM Plex Mono", Courier, Menlo, Consolas, monospace;
+  text-decoration: none;
+  padding: 5px;
 }
 
-#menu>div:hover {
-    transform: scale(1.1, 1.0);
+#menu > div:hover {
+  transform: scale(1.1, 1);
 }
 </style>
